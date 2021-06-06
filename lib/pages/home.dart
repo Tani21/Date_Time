ee47cdfae0194d50dcb972a891d762657622afa2
snapsid <siddharthchopra555@gmail.com>
2021-06-06 15:35:21 +0530

    fetching data fixed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,39 +1,48 @@
+import 'package:date_and_time/pages/location.dart';
 import 'package:flutter/material.dart';
 
-
 class Home extends StatefulWidget {
-  
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  // Map data = {};
+  var loc = "";
+  var flag = "";
+  var time = "";
+  @override
+  void initState() {
+    loc = Location.loc1;
+    flag = Location.flag1;
+    time = Location.time1;
 
-Map data = {};
-
+    print('loc: $loc');
+    print('flag: $flag');
+    print('time: $time');
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-data = ModalRoute.of(context).settings.arguments;
-print(data);
- 
+    // data = ModalRoute.of(context).settings.arguments;
+    // print("data $data");
+
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: <Widget>[
-            FloatingActionButton.extended(onPressed: (){
-              Navigator.pushNamed(context,'/');
-            },
-            icon: const Icon(Icons.edit_location),
-            label: const Text('Edit location'),
+            FloatingActionButton.extended(
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+              icon: const Icon(Icons.edit_location),
+              label: const Text('Edit location'),
             )
           ],
         ),
       ),
-        
-      
-      
     );
   }
 }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,7 +1,7 @@
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class WorldTime{
+class WorldTime {
   String location;
   String time;
   String flag;
@@ -10,25 +10,24 @@ class WorldTime{
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    
-   // make the request
-   final response = await http.get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-   Map data = jsonDecode(response.body);
-   //print(data);
-  
-  // get properties from data
-  String datetime = data['datetime'];
-  String offset = data['utc_offset'].substring(1,3);
-  //String part_offset = offset.s
-  // print(datetime);
-  // print(offset);
+    // make the request
+    final response =
+        await http.get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+    Map data = jsonDecode(response.body);
+    //print(data);
 
-  // create a datetime object
-  DateTime now = DateTime.parse(datetime);
-  now=now.add(Duration(hours:int.parse(offset)));
-  //print(now);
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    //String part_offset = offset.s
+    // print(datetime);
+    // print(offset);
 
-  time = now.toString();
+    // create a datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    //print(now);
+
+    time = now.toString();
+  }
 }
-}
-
